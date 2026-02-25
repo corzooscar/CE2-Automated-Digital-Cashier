5003680ca529704b0f54f72e0ae27c389fadb859
Johns8729 <jhonsebastian8729@gmail.com>
2026-02-25 10:25:37 -0500

    Add additional print statements to docdepruebas.py

--- docdepruebas.py
+++ docdepruebas.py
@@ -2,5 +2,8 @@ print("Este es un documento de prueba, todos pueden editarlo")
 print("hace hambre")
 print("aro")
 print ("sisas")
-print("all girl the same")
+print("all girl the same") 
+print("lucid dreams")
+print("robbery")
+
 
