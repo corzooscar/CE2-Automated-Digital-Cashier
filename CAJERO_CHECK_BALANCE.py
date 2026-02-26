da8b1da64fa49c11b94eee29558f2236f5b9ad24
silvio1615 <silvio_luis15@live.com>
2026-02-26 07:48:02 -0500

    agregar fecha a la consulta

--- CAJERO_CHECK_BALANCE.py
+++ CAJERO_CHECK_BALANCE.py
@@ -1,6 +1,9 @@
+import datetime
+import time
 saldo_inicial = 1000.0
 
 opcion=int (input("cuantas operaciones desea relizar: "))
+cant_opera=0
 while True:
     print("menu de opciones: ")
     print("1. consultar saldo")
@@ -12,16 +15,22 @@ while True:
     
     # OPCION 1
 
-    if opcion == 1:
+    if opcion_2== 1:
 
+        fecha_actual = datetime.date.today()
+        hora_actual = time.strftime("%H:%M:%S")
+        print("fecha: ",fecha_actual)
+        print("hora: ",hora_actual)
         print("Tu saldo actual es: ", saldo_inicial )
         print("\n") 
-        print("silvio")
+        
+
     
     if opcion_2 == 2:
         retirar_dinero = float(input("cuanto desea retirar?:"))
         if retirar_dinero>saldo_inicial:
             print("no tienes fondos suficientes")
+            cant_opera=+1
         else :
             saldo_inicial-=retirar_dinero
             print("su nuevo saldo es: ",saldo_inicial,"su monto retirado fue: ",retirar_dinero)
