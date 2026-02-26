66e43ffcdd774608fd6237c6f4ab92e5eee56304
Juanesco100 <juanse01sebas@gmail.com>
2026-02-26 07:23:54 -0500

    money depositi v2

--- simulador_cajero.py
+++ simulador_cajero.py
@@ -5,4 +5,8 @@
 if accion == 3:
 
         deposito = int(input("Ingrese el monto a depositar: "))
+        
+        while deposito < 0
+         deposito=int(input("Error, ingrese un valor positivo: "))
+         
        
