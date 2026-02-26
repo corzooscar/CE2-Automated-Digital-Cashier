f4b53f358846b73798e011e06aa92769ac6357cb
jesus bustillo <jesusbustillo414@gmail.com>
2026-02-26 07:26:22 -0500

    money deposit v3

--- simulador_cajero.py
+++ simulador_cajero.py
@@ -6,7 +6,13 @@ if accion == 3:
 
         deposito = int(input("Ingrese el monto a depositar: "))
         
-        while deposito < 0
+        while deposito < 0:
          deposito=int(input("Error, ingrese un valor positivo: "))
+
+         if deposito > 0:
+            print ("Su nuevo saldo es: ", saldo+deposito)
+
+
+print(" Gracias por usar el cajero automatico")
          
        
