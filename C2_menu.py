550f008f9147ac194a1b25545dd2ad689fb92328
john <jhonsebastian8729@gmail.com>
2026-02-26 09:38:20 -0500

    C2_menu V2

--- C2_menu.py
+++ C2_menu.py
@@ -1,10 +1,9 @@
 saldo = 1000
-print("Bienvenido al cajero Automatico")
-operaciones = int(input("que operacion desea realizar:"))
-for i in range(operaciones): int(input("\n 1.Consultar saldo \n 2.depositar dinero\n 3.retirar dinero"))
-while operaciones <1 or operaciones >3:
-    int(input("operacion invalida, ingrese nuevamente."))
+Cant_Operaciones = int(input(" --- ¡Bienvenido al Cajero Automático! ---\n ¿Cuántas operaciones desea realizar?:  "))
 
-
-    
-    
+for i in range (Cant_Operaciones) :
+    Conteo = i + 1
+    Opcion = int(input(f"\n ------------ Menú Principal ------------#{Conteo}  \n 1 → Consultar saldo \n 2 → Retirar dinero \n 3 → Depositar dinero \n 4 → Finalizar \n ➤ "))
+    while Opcion < 1 or Opcion > 4:
+        print("\nERROR: Por favor ingrese una opción válida")
+        Opcion = int(input(f"\n ------------ Menú Principal ------------ | #{Conteo}  \n 1 → Consultar saldo \n 2 → Retirar dinero \n 3 → Depositar dinero \n 4 → Finalizar \n ➤ "))
