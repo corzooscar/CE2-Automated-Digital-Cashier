50d1db1cd566b738f7c9a3ae59ff4392ba72e20c
JromeroRodriguez <josephteck123@gmail.com>
2026-02-26 09:14:35 -0500

    Withdrawal_V1

---
+++ C2_feature_withdraw_money.py
@@ -0,0 +1,19 @@
+elif opcion == 2:
+    try:
+        monto_retirar = int(input("\ningrese el monto a retirar: "))
+    except ValueError:
+        print("\nMonto no válido. Por favor, ingrese un número entero.\n")
+        continue
+    while monto_retirar < 0:
+        print("\nMonto no válido. Por favor, ingrese un monto positivo.\n")
+        try:
+            monto_retirar = int(input("ingrese el monto a retirar: "))
+        except ValueError:
+            print("\nMonto no válido. Por favor, ingrese un número entero.\n")
+            continue
+    if monto_retirar > saldo:
+        print("\nFondos insuficientes\n")
+    else:
+        saldo -= monto_retirar
+        print(F"\nRetiro exitoso. Su nuevo saldo actualizado es de: {saldo}\n")
+        print("\nGracias por usar el cajero automático\n")
